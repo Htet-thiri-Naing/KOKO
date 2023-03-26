b39b9b723ceafa187b61a1c965b88cdb9d616d9c
Margo_Nile <76113329+Htet-thiri-Naing@users.noreply.github.com>
2023-03-26 22:27:13 +0630

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	let name = 'world';
+	let myflavor;
+	let bought = false;
+	let flavors = 	['strawberry','chocolate','vanilla','lime'];
+	function buyIcecream(){
+		  bought = true;	  
+	}
+</script>
+
+{#if bought}
+<p>
+	You have bought {myflavor}
+</p>
+{:else}
+	Please choose any flavor!<br><br>
+<select bind:value={myflavor}>
+	{#each flavors as flavor}
+	<option value={flavor}>{flavor} flavor</option>
+	{/each}
+</select>
+<button on:click={buyIcecream}>
+	Buy
+</button>
+{/if}
